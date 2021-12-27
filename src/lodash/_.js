2c015d7668dce225996004c4bf2138d3d5265d5e
Felipe <fepipe82@hotmail.com>
2021-12-27 15:31:07 -0300

    order _.js

--- src/lodash/_.js
+++ src/lodash/_.js
@@ -3,7 +3,8 @@ const _ = {
       const lowerClampedValue = Math.max(number, lower);
       const clampedValue = Math.min(lowerClampedValue, upper);
       return clampedValue;
-  }, inRange(number, start, end) {
+  }, 
+  inRange(number, start, end) {
       if ( end === undefined ) {
         end = start;
         start = 0;
@@ -15,10 +16,12 @@ const _ = {
       }
       const isInRange = (number >= start && number < end);
       return isInRange;
-  }, words(string) {
+  }, 
+  words(string) {
      const words = string.split(' ');
      return words;
-  }, pad(string, length) {
+  }, 
+  pad(string, length) {
      if (string.length >= length) {
        return string;
      }
@@ -27,18 +30,20 @@ const _ = {
      const endingPadding = totalPadding - beginingPadding;
 
      return ' '.repeat(beginingPadding) + string + ' '.repeat(endingPadding);
-  }, has(object, key) {
-    
+  }, 
+  has(object, key) {    
     if (typeof object !== 'object') {
       return false;
     } else {
       return Object.keys(object).some(el => el === key);
     }
-  }, invert(object){
+  }, 
+  invert(object){
      const objectInvert = {};
      Object.entries(object).forEach( el => objectInvert[el[1]] = el[0]);
      return objectInvert;
-  }, findKey(object, predicate) {
+  }, 
+  findKey(object, predicate) {
       let result;
       Object.entries(object).forEach( el => {
         let value = el[1];
@@ -51,15 +56,18 @@ const _ = {
       if (result) return result;
       
       return undefined;
-  }, drop(array, n=1) {
+  }, 
+  drop(array, n=1) {
      return array.slice(n);
-  }, dropWhile(array, predicate) {
+  }, 
+  dropWhile(array, predicate) {
      const dropNumber = array.findIndex( (el,idx) => {
         return !predicate(el,idx,array)
      });
      const droppedArray = this.drop(array,dropNumber);
      return droppedArray;
-  }, chunk(array, n =1) {
+  }, 
+  chunk(array, n =1) {
      const newArray = [];
      let paso = array;
      do {
